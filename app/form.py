2c7447ebf179d5946b44565e54ce9b8bf508532d
José Nunes <josenunesra@gmail.com>
2020-10-25 18:41:33 -0300

    deleted contact

--- app/form.py
+++ app/form.py
@@ -1,17 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FloatField
-from wtforms.validators import DataRequired, Length, Email
-
-
-class ContactForm(FlaskForm):
-
-    name = StringField('name', [DataRequired()])
-
-    email = StringField('email', [Email(message=('Not a valid email address.')), DataRequired()])
-
-    message = StringField('message', [DataRequired(), Length(min=4, message=('Your message is too short.'))])
-
-    submit = SubmitField('Submit')
+from wtforms import SubmitField, FloatField
+from wtforms.validators import DataRequired
 
 
 class IMCForm(FlaskForm):
--- app/main.py
+++ app/main.py
@@ -1,24 +1,15 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, render_template, request
 
-from app.form import ContactForm, IMCForm
+from app.form import IMCForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'who-let-the-dogs-out'
 
 
 @app.route('/')
-def index(status=None, message=None):
-    return render_template("index.html", status=status, message=message)
+def index():
+    return render_template("index.html")
 
-
-@app.route('/<status>/<message>')
-def send_message(status, message):
-    return render_template("index.html", status=status, message=message)
-
-
-@app.route('/contact', methods=['GET', 'POST'])
-def contact():
-    form = ContactForm()
 
     if request.method == 'POST':
         if form.validate_on_submit():
@@ -38,19 +29,9 @@ def imc_calc():
             imc = round(form.peso.data / (form.altura.data**2), 2)
             return render_template('imc_result.jinja2', imc_result=imc)
         else:
-            return render_template('imc_calc.jinja2', form=form, status='error', message=form.errors)
+            return render_template('imc_calc.jinja2', form=form)
 
-    return render_template('imc_calc.jinja2', form=form, status=None, message=None)
-
-
-@app.route('/test')
-def test():
-    return redirect(url_for('send_message', status="success", message="Mensagem enviada com sucesso!"))
-
-
-@app.route('/test2')
-def test2():
-    return redirect(url_for('send_message', status="error", message=[{"campo": "erro"}]))
+    return render_template('imc_calc.jinja2', form=form)
 
 
 if __name__ == '__main__':
